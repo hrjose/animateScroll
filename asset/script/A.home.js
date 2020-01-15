0fc741a0dad3977a591017e3f19bdba5d3bc67be
hrjose <hrjose_2497@hotmail.com>
2020-01-14 20:43:01 -0600

    funcionalidad de carrusel con lighbox

--- asset/script/A.home.js
+++ asset/script/A.home.js
@@ -48,3 +48,17 @@ var scena3 = new ScrollMagic.Scene({
 
     AOS.init();
     //scena.destroy(true); para destruir la scena
+
+    $('.portfolio-slides').slick({
+        infinite        : true,
+        slidesToShow    : 3,
+        slidesToScroll  : 1,
+        mobileFirst     : true
+      });
+      
+      
+      $('.portfolio-slides').slickLightbox({
+        itemSelector        : 'a',
+        navigateByKeyboard  : true
+      });
+
